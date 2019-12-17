52ad7cf8c1b3a9416a02668be5043138487bbb51
5argon <pyasry@gmail.com>
2019-12-17 22:18:52 +0700

    Fix bugs

--- EntityManagerUtility.cs
+++ EntityManagerUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
@@ -15,10 +16,12 @@ namespace E7.EcsGadgets
     public class EntityManagerUtility
     {
         EntityManager em;
-
+        List<int> scdIndices;
+        
         public EntityManagerUtility(World world)
         {
             this.em = world.EntityManager;
+            scdIndices = new List<int>();
         }
 
         /// <summary>
@@ -112,15 +115,18 @@ namespace E7.EcsGadgets
         /// Performs a linear search over all existing SCD of a single type to find out the index.
         /// The search equality test is by List.IndexOf
         /// Remember that the index may become unusable once the SCD order version changed.
-        ///
-        /// This method allocates a List and throw it away, generating garbage.
         /// </summary>
+        /// <param name="scdValues">In order for this method to not generate garbage, please bring your own
+        /// managed List of the type you want that you allocate on OnCreate in your system. Just bring it in,
+        /// this method has a .Clear inside.</param>
         /// <returns>-1 when not found. That means this SCD value has not been used anywhere yet or not used anymore.</returns>
-        public int IndexOfSharedComponentData<T>(T sharedComponentValue) where T : struct, ISharedComponentData
+        public int IndexOfSharedComponentData<T>(T sharedComponentValue, List<T> scdValues) where T : struct, ISharedComponentData
         {
-            List<T> searchList = new List<T>();
-            em.GetAllUniqueSharedComponentData(searchList);
-            return searchList.IndexOf(sharedComponentValue);
+            scdValues.Clear();
+            scdIndices.Clear();
+            em.GetAllUniqueSharedComponentData(scdValues, scdIndices);
+            var indexOf = scdValues.IndexOf(sharedComponentValue);
+            return indexOf == -1 ? -1 : scdIndices[indexOf];
         }
     }
 }
--- SharedComponentCommandBuffer.cs
+++ SharedComponentCommandBuffer.cs
@@ -5,39 +5,45 @@ using Unity.Entities;
 namespace E7.EcsGadgets
 {
     /// <summary>
-    /// You can set SCD to an entity in job, and it is burstable unlike
+    /// You can defer set SCD to an entity in job, and it is burstable unlike
     /// the one on ECB.
-    /// .
-    /// It is just that you have to ensure your SCD is componsed of
+    /// It is just that you have to ensure your SCD is composed of
     /// only unmanaged fields on your own. You need this per one type of SCD
     /// since all SCD API are gated tightly by generic typing.
     /// </summary>
-    internal struct SharedComponentCommandBuffer<T> : IDisposable
+    public struct SharedComponentCommandBuffer<T> : IDisposable
         where T : struct, ISharedComponentData
     {
-        NativeList<T> scdValues;
-        NativeList<Entity> targetEntity;
+        [ReadOnly] NativeQueue<T> scdValues;
+        [ReadOnly] NativeQueue<Entity> targetEntity;
+        
+        //When this go into jobs the thread index would be set on these two.
+        NativeQueue<T>.ParallelWriter scdValuesW;
+        NativeQueue<Entity>.ParallelWriter targetEntityW;
+        
         public SharedComponentCommandBuffer(Allocator allocator)
         {
-            scdValues = new NativeList<T>(4,allocator);
-            targetEntity = new NativeList<Entity>(4,allocator);
+            scdValues = new NativeQueue<T>(allocator);
+            targetEntity = new NativeQueue<Entity>(allocator);
+            scdValuesW = scdValues.AsParallelWriter();
+            targetEntityW = targetEntity.AsParallelWriter();
         }
 
         /// <summary>
         /// Enqueue command by remembering the entity and SCD.
         /// SCD must only contains unmanaged types.
         /// </summary>
-        public void SetComponentData(Entity e, T sharedComponentData)
+        public void SetSharedComponent(Entity e, T sharedComponentData)
         {
-            scdValues.Add(sharedComponentData);
-            targetEntity.Add(e);
+            scdValuesW.Enqueue(sharedComponentData);
+            targetEntityW.Enqueue(e);
         }
 
         public void Playback(EntityManager em)
         {
-            for (int i = 0; i < scdValues.Length; i++)
+            while (scdValues.Count > 0 && targetEntity.Count > 0)
             {
-                em.SetSharedComponentData(targetEntity[i], scdValues[i]);
+                em.SetSharedComponentData(targetEntity.Dequeue(), scdValues.Dequeue());
             }
         }
 
